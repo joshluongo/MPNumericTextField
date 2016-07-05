8186e67bc19b0dd02d449895c338198a31ed5735
Joshua Luongo <josh@jrapps.com.au>
2016-07-05 12:01:38 +1000

    Changed project name, due to naming conflict.

---
+++ MPNumericField/MPNumericField/MPNumericField.h
@@ -0,0 +1,21 @@
+//
+//  MPNumericField.h
+//  MPNumericField
+//
+//  Created by Joshua Luongo on 5/07/2016.
+//  Copyright © 2016 Joshua Luongo. All rights reserved.
+//
+
+@import UIKit;
+
+//! Project version number for MPNumericField.
+FOUNDATION_EXPORT double MPNumericFieldVersionNumber;
+
+//! Project version string for MPNumericField.
+FOUNDATION_EXPORT const unsigned char MPNumericFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPNumericField/PublicHeader.h>
+
+#import <MPNumericTextField/MPFormatterUtils.h>
+#import <MPNumericTextField/MPNumericTextField.h>
+#import <MPNumericTextField/MPNumericTextFieldDelegate.h>
